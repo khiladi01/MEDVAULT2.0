2b7fa34e8a7e8b78d0e1db1adb7c4e1e803755de
khiladi01 <akshayraj3435@gmail.com>
2025-09-11 22:01:15 +0530

    fix(next.config.mjs): update API base URL for frontend-backend integration

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*", // backend target
+        destination: "https://medvaultservice.onrender.com/api/:path*", // backend target
       },
     ];
   },
